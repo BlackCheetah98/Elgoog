2137204902bceb12ea82897f94f4216a06da7cbc
BlackCheetah98 <53140855+BlackCheetah98@users.noreply.github.com>
2019-10-16 18:00:23 +0530

    Add files via upload

---
+++ trigger_obj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class trigger_obj : MonoBehaviour
+{
+    public int a1, a2, b1, b2;
+    public Text text_component;
+    public float countdown;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            SceneManager.LoadScene(0);
+            countdown = 5;
+        }
+        countdown -= Time.deltaTime;
+        text_component.text = Mathf.Round(countdown).ToString();
+    }
+}
